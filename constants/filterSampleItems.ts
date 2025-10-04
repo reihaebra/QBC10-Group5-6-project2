8cd1a73f7143b41ab77d644b3e5c03419bdc121c
Hanna <hannahazratian@gmail.com>
2025-10-04 20:00:35 +0330

    feat(constants): add filterSampleItems for Filter component

---
+++ constants/filterSampleItems.ts
@@ -0,0 +1,15 @@
+type FilterSampleItems = {
+  id: number;
+  name: string;
+};
+
+export const samples: FilterSampleItems[] = [
+  { id: 1, name: "Smartphones" },
+  { id: 2, name: "Laptops" },
+  {
+    id: 3,
+    name: "Tablets",
+  },
+  { id: 4, name: "Wearables" },
+  { id: 5, name: "Audio" },
+];
