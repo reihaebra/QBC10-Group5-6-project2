adf7abce0fb3053cfb8f29669ce221cf57b34d0b
AhmadEmrani <mrany393@gmail.com>
2025-10-07 20:31:39 +0330

    resolve(ShopPage):button element resolved

--- src/components/ShopPageButton.tsx
+++ src/components/ShopPageButton.tsx
@@ -4,7 +4,7 @@ interface ShopPageButton {
 }
 const ShopPage_button: React.FC<ShopPageButton> = ({ children }) => {
   return (
-    <button className="w-60 px-[65px] max-h-10 py-2 text-center bg-on-primary-light text-black rounded-[9999px] text-[16px] cursor-pointer">
+    <button className="w-60 px-[65px] max-h-10 py-2 text-center bg-on-primary-light text-black  rounded-full text-base cursor-pointer">
       {children}
     </button>
   );
--- src/components/ShopPageProducts.tsx
+++ src/components/ShopPageProducts.tsx
@@ -1,54 +1,5 @@
 import ShopProductCard from "./ui/ShopProductCard";
-interface Product {
-  title: string;
-  brand: string;
-  price: string;
-  description: string;
-  imageUrl: string;
-}
-
-const data: Product[] = [
-  {
-    title: "iPhone 14 Pro",
-    brand: "Apple",
-    price: "۱۰,۰۰۰ تومان",
-    description:
-      "آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر Retina XDR ...",
-    imageUrl: "./src/assets/images/iphone-14-pro.png",
-  },
-  {
-    title: "iPhone 14 Pro",
-    brand: "Apple",
-    price: "۱۰,۰۰۰ تومان",
-    description:
-      "آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر Retina XDR ...",
-    imageUrl: "./src/assets/images/iphone-14-pro.png",
-  },
-  {
-    title: "iPhone 14 Pro",
-    brand: "Apple",
-    price: "۱۰,۰۰۰ تومان",
-    description:
-      "آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر Retina XDR ...",
-    imageUrl: "./src/assets/images/iphone-14-pro.png",
-  },
-  {
-    title: "iPhone 14 Pro",
-    brand: "Apple",
-    price: "۱۰,۰۰۰ تومان",
-    description:
-      "آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر Retina XDR ...",
-    imageUrl: "./src/assets/images/iphone-14-pro.png",
-  },
-   {
-    title: "iPhone 14 Pro",
-    brand: "Apple",
-    price: "۱۰,۰۰۰ تومان",
-    description:
-      "آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر Retina XDR ...",
-    imageUrl: "./src/assets/images/iphone-14-pro.png",
-  },
-];
+import data from "../../constants/ShopPageSamples";
 
 const ShopPage_Products = () => {
   return (
