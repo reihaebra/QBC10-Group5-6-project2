5bec2466b654b237018563ad54fb2bf6f07232a5
AhmadEmrani <mrany393@gmail.com>
2025-10-09 00:22:10 +0330

    feat(AllProducts): dark mode for elements

--- src/components/AllProductsCard.tsx
+++ src/components/AllProductsCard.tsx
@@ -16,18 +16,18 @@ const AllProductsCard: React.FC<ProductCardProps> = ({
   imageUrl,
 }) => {
   return (
-    <div className="flex flex-row p-2 rounded-lg max-w-[672px] h-fit bg-card-light">
+    <div className="flex flex-row p-2 rounded-lg max-w-[672px] h-fit bg-card-light dark:bg-[var(--color-shop-card-dark)]">
       <img src={imageUrl} alt={title} className="W-[160PX] h-[160px]" />
       <div className="flex flex-col p-4">
         <div className="flex justify-between">
-          <h2 className="pb-2 text-xl font-bold text-primary-text-light">
+          <h2 className="pb-2 text-xl font-normal text-primary-text-light dark:text-white">
             {title}
           </h2>
           <p className="text-xs font-normal text-secondary-light">
             ۳۱ مرداد ۱۴۰۳
           </p>
         </div>
-        <p className="pb-4 text-sm font-normal text-secondary-light ">
+        <p className="pb-4 text-sm font-normal text-secondary-light dark:text-[var(--color-secondary-dark)] ">
           {description}
         </p>
         <div className="flex flex-row justify-between">
@@ -36,7 +36,7 @@ const AllProductsCard: React.FC<ProductCardProps> = ({
             handleClick={handleClick}
             iconSrc="./src/assets/icons/left-arrow.svg"
           />
-          <p className="text-base text-primary-text-light font-normal">{price}</p>
+          <p className="text-base text-primary-text-light font-normal dark:text-white">{price}</p>
         </div>
       </div>
     </div>
