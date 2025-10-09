175c4f2218b2b5d8afa51eb1597170482648b0cd
Hana <133277340+hnahz@users.noreply.github.com>
2025-10-09 21:18:24 +0330

    fix: remove additional codes from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import UsersPage from "./pages/UsersPage";
 
-const App = () => {
-  return <UsersPage />;
-};
+const App = () => {};
 
 export default App;
