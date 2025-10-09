b0ac4d42ef99bdd9f95d719d6c3846547e6dada4
Hana <133277340+hnahz@users.noreply.github.com>
2025-10-09 21:50:08 +0330

    fix: remove additional codes from ProductPage.tsx

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,11 +1,1 @@
-import ProductForm from '../components/ProductForm';
 
-const ProductPage = () => {
-  return (
-    <div className="min-h-screen bg-(--color-background-base-light) flex items-center justify-center p-8 dark:bg-(--color-background-primary-dark)">
-      <ProductForm />
-    </div>
-  );
-};
-
-export default ProductPage;
