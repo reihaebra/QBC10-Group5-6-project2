a7b4c77557eae593a38e9f48c8855c295237beb0
parmis.asadi <asadipour84@gmail.com>
2025-10-09 21:27:03 +0330

    refactor: fixing layout problems

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -4,7 +4,7 @@ import SummarySection from './SummarySection'
 
 const Summary = () => {
   return (
-    <div className='w-[80%] flex flex-col gap-[36px]'>
+    <div className='w-[80%] flex flex-col gap-[36px] mb-15'>
       <CartTable/>
       <SummarySection/>
     </div>
