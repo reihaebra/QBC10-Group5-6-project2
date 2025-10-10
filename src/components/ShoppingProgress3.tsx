25e714b2c4a6a7ce313b012ac0e346664ddf65d9
parmis.asadi <asadipour84@gmail.com>
2025-10-10 22:06:36 +0330

    refactor: fixing requested changes in review

--- src/components/ShoppingProgress3.tsx
+++ src/components/ShoppingProgress3.tsx
@@ -6,7 +6,7 @@ import PurchaseSteps3 from './PurchaseSteps3'
 const ShoppingProgress3 = () => {
   return (
     <>
-        <main className="w-full h-full bg-background-base-light dark:bg-[var(--color-background-primary-dark)] mr-25 flex flex-col justify-center items-center pt-10 gap-18 font-yekan-bakh">
+        <main className="w-full h-full bg-background-base-light dark:bg-[var(--color-background-primary-dark)] mr-25 flex flex-col justify-center items-center mt-12 gap-18 font-yekan-bakh">
           <PurchaseSteps3/>
           <Summary/>
         </main>
