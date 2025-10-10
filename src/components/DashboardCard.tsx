68ccc4eeb392e78fc34f4388846d38f6e6beeb06
AhmadEmrani <mrany393@gmail.com>
2025-10-10 17:06:02 +0330

    feat(DashboardCard): dark mode for cards

--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -5,11 +5,11 @@ interface DashboardCardProps {
 
 const DashboardCard: React.FC<DashboardCardProps> = ({title , amount}) => {
   return (
-    <div className="w-80 h-40 rounded-lg bg-on-primary-light flex flex-col justify-center gap-4 pr-7">
+    <div className="w-80 h-40 rounded-lg bg-on-primary-light flex flex-col justify-center gap-4 pr-7 dark:bg-[var(--color-primary-text-light)]">
       <figure className="w-12 h-12 rounded-full bg-primary-main py-3 px-5 text-white">$</figure>
       <div className="flex flex-col gap-1">
-        <p className="font-normal text-base text-secondary-light">{title}</p>
-        <p className="font-bold text-xl text-primary-text-light">{amount}</p>
+        <p className="font-normal text-base text-secondary-light dark:text-[var(--color-secondary-dark)]">{title}</p>
+        <p className="font-bold text-xl text-primary-text-light dark:text-[var(--color-on-primary-ligh)]">{amount}</p>
       </div>
     </div>
   )
