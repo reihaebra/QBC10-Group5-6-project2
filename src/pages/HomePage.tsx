9b9f594438d9831306cfc4bb2218a32771600f4f
nima <nimawindstorm@gmail.com>
2025-10-10 15:42:32 +0330

    final

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,1 @@
-import React from "react";
-import HeroSection from "../components/HeroSection";
-import SpecialProducts from "../components/SpecialProducts";
 
-const HomePage = () => {
-  return (
-    <main className="flex flex-col items-center bg-neutral-light-100 dark:bg-neutral-dark-100">
-      {/* 🔹 بخش بالای صفحه */}
-      <HeroSection />
-
-      {/* 🔹 بخش محصولات ویژه */}
-      <SpecialProducts />
-    </main>
-  );
-};
-
-export default HomePage;
