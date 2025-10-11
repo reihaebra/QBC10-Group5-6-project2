dafe3339c3acbc095b82139374baf2f185e960b4
parmis.asadi <asadipour84@gmail.com>
2025-10-11 19:04:34 +0330

    fix: final changes

--- src/components/ShoppingProgress3.tsx
+++ src/components/ShoppingProgress3.tsx
@@ -5,12 +5,10 @@ import PurchaseSteps3 from './PurchaseSteps3'
 
 const ShoppingProgress3 = () => {
   return (
-    <>
-        <main className="w-full h-full bg-background-base-light dark:bg-[var(--color-background-primary-dark)] mr-25 flex flex-col justify-center items-center mt-12 gap-18 font-yekan-bakh">
-          <PurchaseSteps3/>
-          <Summary/>
-        </main>
-    </>
+    <main className="flex-1 h-screen bg-background-base-light dark:bg-[var(--color-background-primary-dark)] mr-20 flex flex-col justify-center items-center pt-12 gap-18 font-yekan-bakh ">
+      <PurchaseSteps3/>
+      <Summary/>
+    </main>
   )
 }
 
