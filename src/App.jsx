4db5cf24c618fb2aace429a25983273df4716511
parmis.asadi <asadipour84@gmail.com>
2025-10-13 01:48:55 +0330

    fix: emptying app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import ShoppingProgressPage from "./pages/ShoppingProgressPage";
 
-const App = () => {
-    return (
-        <ShoppingProgressPage/>
-    )
-};
+const App = () => {};
 
 export default App;
