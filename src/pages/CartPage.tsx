8dea9e25d729992c9eb4456e96f2cfb0c2208627
Hana <133277340+hnahz@users.noreply.github.com>
2025-10-15 00:21:41 +0330

    feat: add UserDropdown component to Sidebar

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,11 +1,14 @@
 import Cart from "../components/Cart";
 import Sidebar from "../components/ui/Sidebar";
+import UserDropdown from "../components/ui/UserDropdown";
 
 const CartPage = () => {
   return (
     <>
       <div className="relative flex h-screen justify-between dark:bg-[var(--color-background-primary-dark)] ">
-        <Sidebar />
+        <Sidebar>
+          <UserDropdown />
+        </Sidebar>
         <div className="relative flex w-screen  mr-24  px-10 mx-a">
           <Cart />
         </div>
