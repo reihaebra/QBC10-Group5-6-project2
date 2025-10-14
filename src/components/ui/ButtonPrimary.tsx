4c99f18f5bed84b0037f9e05900abc5b81f8d87f
Hanna <hannahazratian@gmail.com>
2025-10-14 22:32:05 +0330

    refactor: add bgHoverColor prop

--- src/components/ui/ButtonPrimary.tsx
+++ src/components/ui/ButtonPrimary.tsx
@@ -3,6 +3,7 @@ interface ButtonPrimaryProps {
   iconSrc?: string;
   altText?: string;
   bgColor?: string;
+  bgHoverColor?: string;
   handleClick: () => void;
 }
 
@@ -11,12 +12,13 @@ const ButtonPrimary = ({
   iconSrc,
   altText = "",
   bgColor = "bg-primary-main",
+  bgHoverColor = "bg-primary-dark",
   handleClick,
 }: ButtonPrimaryProps) => {
   return (
     <button
       onClick={handleClick}
-      className={`flex items-center gap-2 px-3 py-2 rounded-lg cursor-pointer hover:bg-primary-dark ${bgColor}`}
+      className={`flex items-center gap-2 px-3 py-2 rounded-lg cursor-pointer hover:${bgHoverColor} ${bgColor}`}
     >
       <span className="font-normal text-sm text-on-primary-light">{text}</span>
       {iconSrc && <img src={iconSrc} alt={altText} />}
