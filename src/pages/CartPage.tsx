0afa5de7217361845a20988e7b3c1201fecd8f08
Hamed <vafaei.hamed@gmail.com>
2025-10-14 23:56:10 +0330

    feat(CartPage): force commit CartPage,Cart & All of component and sample data

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import Cart from "../components/Cart";
+import Sidebar from "../components/ui/Sidebar";
 
-export default function CartPage() {
-  return <div>CartPage</div>;
-}
+const CartPage = () => {
+  return (
+    <>
+      <div className="relative flex h-screen justify-between dark:bg-[var(--color-background-primary-dark)] ">
+        <Sidebar />
+        <div className="relative flex w-screen  mr-24  px-10 mx-a">
+          <Cart />
+        </div>
+      </div>
+    </>
+  );
+};
+export default CartPage;
