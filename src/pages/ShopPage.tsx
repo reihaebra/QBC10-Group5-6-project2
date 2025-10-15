cbe314f66afa11dcc65eb7922d2fce06e8840081
Hana <133277340+hnahz@users.noreply.github.com>
2025-10-15 20:51:13 +0330

    fix: add UserDropdown

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,10 +1,14 @@
 import Sidebar from "../components/ui/Sidebar";
 import ShowPageAside from "../components/ShopPageAside";
 import ShopPageProducts from "../components/ShopPageProducts";
+import UserDropdown from "../components/ui/UserDropdown";
+
 const ShopPage = () => {
   return (
-    <div className="flex  bg-background-base-light justify-between font-yekan-bakh dark:bg-[var(--color-background-primary-dark)]">
-      <Sidebar />
+    <div className="flex bg-background-base-light justify-between font-yekan-bakh dark:bg-[var(--color-background-primary-dark)]">
+      <Sidebar>
+        <UserDropdown />
+      </Sidebar>
       <div className="relative flex flex-row gap-16 top-8 justify-center font-YekanBakh text-black bg-background-base-light h-auto w-[calc(100vw-156px)] dark:bg-[var(--color-background-primary-dark)] dark:text-white">
         <ShowPageAside />
         <ShopPageProducts />
