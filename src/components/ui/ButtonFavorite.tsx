6c35d3230f88c960b44629d41289e5633b316504
AhmadEmrani <mrany393@gmail.com>
2025-10-15 22:35:52 +0330

    fix: favorite button

--- src/components/ui/ButtonFavorite.tsx
+++ src/components/ui/ButtonFavorite.tsx
@@ -1,10 +1,11 @@
+
 const ButtonFavorite = () => {
   return (
     <button className="flex items-center justify-center absolute top-4 right-4 cursor-pointer">
       <img
-        src="../../public/icons/favorite.svg"
+        src="../../../public/icons/favorite.svg"
         alt="favorite"
-        className="hover:content-[url('./../../public/icons/favorite-hover.svg')]"
+        className="hover:content-[url('../../../public/icons/favorite-hover.svg')]"
       />
     </button>
   );
