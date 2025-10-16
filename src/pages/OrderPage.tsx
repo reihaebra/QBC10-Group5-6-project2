81ca1a4f194d87bc0d0070c539eced0712e0c6b9
Hanna <hannahazratian@gmail.com>
2025-10-16 20:22:40 +0330

    refactor: update OrderPage to match Figma design

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -3,16 +3,17 @@ import OrdersFrame from "../components/OrdersFrame";
 import AdminDropdown from "../components/ui/AdminDropdown";
 const OrderPage = () => {
   return (
-    <>
-      <div className="relative flex h-screen justify-between bg-background-base-light dark:bg-[var(--color-background-primary-dark)] ">
-        <Sidebar>
-          <AdminDropdown />
-        </Sidebar>
-        <div className="relative flex w-screen  mr-24 mt-40  px-10 mx-a">
-          <OrdersFrame />
-        </div>
+    <div
+      className="relative flex min-h-screen h-full justify-between 
+      bg-background-base-light dark:bg-[var(--color-background-primary-dark)] "
+    >
+      <Sidebar>
+        <AdminDropdown />
+      </Sidebar>
+      <div className="relative flex w-screen py-8 pl-10 pr-32">
+        <OrdersFrame />
       </div>
-    </>
+    </div>
   );
 };
 
