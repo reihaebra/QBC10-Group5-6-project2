d45c4e37086f59ac4ad9b06781e2f456b1b8693e
mojtaba139797 <mojtabahabibi139797@gmail.com>
2025-10-19 23:53:11 +0330

    feat: api logic for singleproduct and related products

--- src/components/ProductContainer.tsx
+++ src/components/ProductContainer.tsx
@@ -1,72 +1,43 @@
 import ButtonPrimary from "./ui/ButtonPrimary";
 import InventoryDropdown from "../components/ui/InventoryDropdown";
 import ButtonFavorite from "./ui/ButtonFavorite";
-
 import { useState } from "react";
-import { getSingleProducts } from "../api/requests/singleProduct";
+import type { Product } from "../pages/ProductPage";
+import page from "../../public/icons/star-light.svg";
 
-interface Reviews {
+interface productCategory {
+  _id: string | number;
   name: string;
-  rating: number | string;
-  comment: string;
-  user: number | string;
-  _id: number | string;
-  createdAt: number | string;
-  updatedAt: string | number;
+  __v: string;
 }
 
-interface Product {
-  id: number | string;
-  name: string;
-  image: string;
-  quantity: number | string;
-  description: string;
-  rating: number | string;
-  price: number | string;
-  countInStock: number | string;
-  reviews: Reviews[];
-  category: string;
-  createdAt: number | string;
-  updatedAt: string | number;
-  numReviews: string | number;
-  __v: number;
+interface ProductContainerProps {
+  product: Product[];
+  productCategory: productCategory[];
 }
 
-const ProductContainer = () => {
-  const [product, setProduct] = useState<Product | null>(null);
-
-  const fetchProduct = async (id: number | string) => {
-    try {
-      const response = await getSingleProducts(id);
-      if (!response.ok) {
-        throw new Error("محصول مورد نظر یافت نشد!");
-      }
-      const data: any = await response.json();
-      setProduct(data);
-    } catch (error) {
-      console.error("Erorr:", error);
-    }
-  };
-
+const ProductContainer = ({
+  product,
+  productCategory,
+}: ProductContainerProps) => {
   return (
     <div className="font-yekan-bakh flex gap-16 bg-background-base-light dark:bg-[var(--color-background-primary-dark)]">
       <div id="productImage" className="w-1/3 rounded-lg">
         <img
-          src="../../public/images/mba13-midnight-select-202402.png"
+          src={product?.image}
           alt="productImage"
           className="w-3xl h-full object-cover rounded-lg"
         />
       </div>
       <div id="productIntroduce" className="flex flex-col gap-11 w-1/3 h-1/3">
         <h3 className="font-semibold text-2xl text-[var(--color-primary-text-light)] dark:text-[var(--color-primary-text-dark)]">
-          Apple MacBook Air M2
+          {product?.name}
         </h3>
         <p className="text-[var(--color-primary-text-light)] font-normal text-base dark:text-[var(--color-primary-text-dark)]">
-          مک بوک ایر با تراشه M2 دارای صفحه نمایش 13.6 اینچی رتینا است. تا 18
-          ساعت عمر باتری و طراحی بدون فن.
+          {product?.description}
         </p>
         <span className="text-3xl font-bold text-[var(--color-primary-text-light)] dark:text-[var(--color-primary-text-dark)]">
-          ۱۰,۰۰۰ تومان
+          {product?.price.toLocaleString()} تومان
         </span>
         <div id="productDetail" className="flex justify-between">
           <div
@@ -87,7 +58,7 @@ const ProductContainer = () => {
               <h6 className="text-[var(--color-secondary-light)] dark:text-[var(--color-secondary-dark)]">
                 امتیاز :
               </h6>
-              <h6>۵</h6>
+              <h6>{product?.rating.toFixed(2)}</h6>
             </div>
             <div className="flex gap-2">
               <img
@@ -103,7 +74,7 @@ const ProductContainer = () => {
               <h6 className="text-[var(--color-secondary-light)] dark:text-[var(--color-secondary-dark)]">
                 تعداد :
               </h6>
-              <h6>۵۲</h6>
+              <h6>{product?.quantity}</h6>
             </div>
             <div className="flex gap-2">
               <img
@@ -119,7 +90,7 @@ const ProductContainer = () => {
               <h6 className="text-[var(--color-secondary-light)] dark:text-[var(--color-secondary-dark)]">
                 موجودی :
               </h6>
-              <h6>۱۰</h6>
+              <h6>{product?.countInStock}</h6>
             </div>
           </div>
           <div
@@ -140,7 +111,7 @@ const ProductContainer = () => {
               <h6 className="text-[var(--color-secondary-light)] dark:text-[var(--color-secondary-dark)]">
                 برند :
               </h6>
-              <h6>اپل</h6>
+              <h6>{productCategory?.name}</h6>
             </div>
             <div className="flex gap-2">
               <img
@@ -156,7 +127,9 @@ const ProductContainer = () => {
               <h6 className="text-[var(--color-secondary-light)] dark:text-[var(--color-secondary-dark)]">
                 زمان بروزرسانی :
               </h6>
-              <h6>چند لحظه قبل</h6>
+              <h6>
+                {new Date(product?.updatedAt).toLocaleDateString("fa-IR")}
+              </h6>
             </div>
             <div className="flex gap-2">
               <img
@@ -172,13 +145,13 @@ const ProductContainer = () => {
               <h6 className="text-[var(--color-secondary-light)] dark:text-[var(--color-secondary-dark)]">
                 نظرات :
               </h6>
-              <h6>۴۲۰۲</h6>
+              <h6>{product?.numReviews}</h6>
             </div>
           </div>
         </div>
         <div className="flex justify-between">
           <div className="flex items-center gap-2 text-[var(--color-primary-text-light)] dark:text-[var(--color-primary-text-dark)]">
-            ۵۰۰۰ نظر
+            {product?.numReviews} نظر
             <div id="stars" className="flex items-center gap-1">
               <img
                 src="../../public/icons/half-star-light.svg"
