49726db68ba6ad573f5dd7a37065a909adc23dfc
Hanna <hannahazratian@gmail.com>
2025-10-21 19:34:34 +0330

    refactor: update Product interface, use Spinner component for loading products, and use react-hot-toast for error handling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,57 +4,63 @@ import HeroSection from "../components/HeroSection";
 import SpecialProducts from "../components/SpecialProducts";
 import Sidebar from "../components/ui/Sidebar";
 import UserDropdown from "../components/ui/UserDropdown";
+import toast from "react-hot-toast";
+import Spinner from "../components/Spinner";
 
-
-export interface Product {
-  id: number;
+interface Product {
+  _id: string;
   name: string;
-  category?: string;
-  price: string;
-  description?: string;
-  imageUrl: string;
+  category: { name: string };
+  price: number | string;
+  description: string;
+  image: string;
   rating?: number;
-  reviews?: number;
-  stock?: number;
-  updatedAt?: string;
+  numReviews?: number;
+  countInStock?: number;
+  updatedAt: string;
 }
 
-
 const HomePage = () => {
-  const [products, setProducts] = useState<any[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchProducts = async () => {
+      const nowTimeStamp = new Date().getTime();
       try {
         const data = await getAllProducts();
         setProducts(data);
       } catch (error) {
-        console.error("خطا در دریافت محصولات:", error);
+        console.error("Error fetching products: ", error);
+        toast.error("خطا در دریافت محصولات. لطفاً دوباره تلاش کنید.");
       } finally {
-        setLoading(false);
+        const elapsed = new Date().getTime() - nowTimeStamp;
+        setTimeout(() => {
+          setLoading(false);
+        }, Math.max(0, 500 - elapsed));
       }
     };
 
     fetchProducts();
   }, []);
 
-  if (loading) return <p className="text-center mt-10">در حال بارگذاری...</p>;
-
-  const heroProducts = products.slice(0, 2);  
+  if (loading) {
+    return <Spinner />;
+  }
+  const heroProducts = products.slice(0, 2);
   const sideProducts = products.slice(2, 6);
   console.log(sideProducts);
-  
+
   const specialProducts = products.slice(6, 12);
 
   return (
     <main className="bg-background-base-light dark:bg-[var(--color-background-primary-dark)] pr-36 py-8 pl-14">
-        <Sidebar>
-          <UserDropdown />
-        </Sidebar>
-        <div>
-      <HeroSection heroProducts={heroProducts} sideProducts={sideProducts} />
-      <SpecialProducts specialProducts={specialProducts} />
+      <Sidebar>
+        <UserDropdown />
+      </Sidebar>
+      <div>
+        <HeroSection heroProducts={heroProducts} sideProducts={sideProducts} />
+        <SpecialProducts specialProducts={specialProducts} />
       </div>
     </main>
   );
