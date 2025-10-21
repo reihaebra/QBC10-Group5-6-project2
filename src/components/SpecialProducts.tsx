208b4eb026871a0a054fa2e3d3ed783fd55bbb53
Hanna <hannahazratian@gmail.com>
2025-10-21 19:30:19 +0330

    refactor: update Product interface, use useNavigate hook, and use id instead of index as key in specialProducts map

--- src/components/SpecialProducts.tsx
+++ src/components/SpecialProducts.tsx
@@ -1,42 +1,46 @@
-import ProductCard from "../components/ui/ProductCard";
-import ButtonSecondary from "../components/ui/ButtonSecondary";
-import { Link } from "react-router-dom";
+import ProductCard from "./ui/ProductCard";
+import ButtonSecondary from "./ui/ButtonSecondary";
+import { useNavigate } from "react-router-dom";
 
-export interface Product {
-  id: number;
-  title: string;
-  brand?: string;
-  price: string;
+interface Product {
+  _id: string;
+  name: string;
+  category?: { name: string };
+  price: number | string;
   description?: string;
-  imageUrl: string;
+  image: string;
   rating?: number;
-  reviews?: number;
-  stock?: number;
+  numReviews?: number;
+  countInStock?: number;
   updatedAt?: string;
 }
-
 
 interface SpecialProductsProps {
   specialProducts: Product[];
 }
 
-const SpecialProducts: React.FC<SpecialProductsProps> = ({ specialProducts }) => {
+const SpecialProducts: React.FC<SpecialProductsProps> = ({
+  specialProducts,
+}) => {
+  const navigateToShop = useNavigate();
+
   return (
     <div className="flex flex-col gap-6 w-full items-center justify-center">
       <div className="flex justify-between items-center py-3 w-full">
         <h2 className="font-yekan-bakh text-xl font-semibold">محصولات ویژه</h2>
-        <Link to={`/user/shop`} className="w-[150px]">
-
-        <ButtonSecondary
-          text="فروشگاه"
-          handleClick={() => {}}
-        />
-        </Link>
+        <div className="font-yekan-bakh">
+          <ButtonSecondary
+            text="فروشگاه"
+            handleClick={() => {
+              navigateToShop("/user/shop");
+            }}
+          />
+        </div>
       </div>
 
       <div className="grid grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 gap-8 h-fit w-fit">
-        {specialProducts.map((product: Product, i: number) => (
-          <ProductCard key={i} product={product} size="big" />
+        {specialProducts.map((product) => (
+          <ProductCard key={product._id} product={product} size="big" />
         ))}
       </div>
     </div>
