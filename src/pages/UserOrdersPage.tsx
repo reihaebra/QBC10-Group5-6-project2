cc4aa129ddfe84ec7cd5efbbcbb8b50029302b77
Hanna <hannahazratian@gmail.com>
2025-10-22 22:07:29 +0330

    feat: add try/catch for async function

--- src/pages/UserOrdersPage.tsx
+++ src/pages/UserOrdersPage.tsx
@@ -4,16 +4,22 @@ import UserDropdown from "../components/ui/UserDropdown";
 import { useEffect, useState } from "react";
 import type { Order } from "./OrderPage";
 import { getAllOrdersMine } from "../api/requests/getAllOrdersMine";
+import toast from "react-hot-toast";
 
 const UserOrdersPage = () => {
   const [orders, setOrders] = useState<Order[]>([]);
-  useEffect(()=>{
-    const fetchData = async ()=> {
-      const response = await getAllOrdersMine();
-      setOrders(response);
-    }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getAllOrdersMine();
+        if (response) setOrders(response);
+      } catch (e) {
+        console.error("Error fetching orders: ", e);
+        toast.error("سفارشی یافت نشد");
+      }
+    };
     fetchData();
-  },[])
+  }, []);
   return (
     <div
       className="relative flex min-h-screen h-full justify-between 
@@ -23,7 +29,7 @@ const UserOrdersPage = () => {
         <UserDropdown />
       </Sidebar>
       <div className="relative flex w-screen py-8 pl-10 pr-32">
-        <OrdersFrame order={orders}/>
+        <OrdersFrame order={orders} />
       </div>
     </div>
   );
