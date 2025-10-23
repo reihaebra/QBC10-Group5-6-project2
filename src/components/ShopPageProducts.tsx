1b7a64ee44b662771b0f9de58015a1c16103c9b4
Hanna <hannahazratian@gmail.com>
2025-10-23 17:09:38 +0330

    feat: add some successful toast messages

--- src/components/ShopPageProducts.tsx
+++ src/components/ShopPageProducts.tsx
@@ -1,6 +1,7 @@
 import ShopProductCard from "./ui/ShopProductCard";
 import Pagination from "./Pagination";
 import { useCartContext } from "../context/useCartContext";
+import toast, { Toaster } from "react-hot-toast";
 
 interface ProductShopPage {
   _id: string;
@@ -36,45 +37,51 @@ const ShopPageProducts = ({
   const { addToCart } = useCartContext()!;
 
   return (
-    <main className="flex flex-col items-center">
-      <section className="flex flex-row flex-wrap gap-6 max-w-[1200px]">
-        {products.length > 0 ? (
-          products.map((item) => (
-            <ShopProductCard
-              key={item._id}
-              product={{
-                _id: item._id,
-                title: item.name,
-                brand: item.category.name,
-                price: item.price,
-                description: item.description,
-                imageUrl: item.image,
-              }}
-              onAddToCart={() => {
-                addToCart({
-                  id: item._id,
+    <>
+      <Toaster position="top-right" />
+      <main className="flex flex-col items-center">
+        <section className="flex flex-row flex-wrap gap-6 max-w-[1200px]">
+          {products.length > 0 ? (
+            products.map((item) => (
+              <ShopProductCard
+                key={item._id}
+                product={{
+                  _id: item._id,
                   title: item.name,
-                  price: Number(item.price),
-                  quantity: 1,
-                  imageUrl: item.image,
+                  brand: item.category.name,
+                  price: item.price,
                   description: item.description,
-                });
-              }}
-            />
-          ))
-        ) : (
-          <p>محصولی یافت نشد.</p>
-        )}
-      </section>
+                  imageUrl: item.image,
+                }}
+                onAddToCart={() => {
+                  addToCart({
+                    id: item._id,
+                    title: item.name,
+                    price: Number(item.price),
+                    quantity: 1,
+                    imageUrl: item.image,
+                    description: item.description,
+                  });
+                  toast.success("محصول با موفقیت به سبد خرید اضافه شد.");
+                }}
+              />
+            ))
+          ) : (
+            <p className="text-primary-text-light dark:text-white font-yekan-bakh font-semibold">
+              محصولی یافت نشد.
+            </p>
+          )}
+        </section>
 
-      {selectedCategories.length === 0 && !priceFilter && hasMore && (
-        <Pagination
-          page={page}
-          totalPages={totalPages}
-          onPageChange={onPageChange}
-        />
-      )}
-    </main>
+        {selectedCategories.length === 0 && !priceFilter && hasMore && (
+          <Pagination
+            page={page}
+            totalPages={totalPages}
+            onPageChange={onPageChange}
+          />
+        )}
+      </main>
+    </>
   );
 };
 
